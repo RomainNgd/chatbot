185997e2733f237a6c4f4baee5936266bc8166e8
JulienHatemian <julien-hat.62@hotmail.fr>
2022-12-16 16:29:24 +0100

    add color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let i = 0;
 
 let input = document.getElementById( 'message' );
+const scroller = document.getElementById( 'message-box' );
+const select = document.getElementById( 'palette' );
 
 input.addEventListener( 'keydown', function(e) {
     if( e.code === "Enter" || e.code === "NumpadEnter" )
@@ -10,6 +12,10 @@ input.addEventListener( 'keydown', function(e) {
             sendMessage();
         }
     }
+} )
+
+palette.addEventListener( 'change', function(){
+    selectColor();
 } )
 
 function openChat()
@@ -33,7 +39,7 @@ function openChat()
         date = date.toLocaleString()
         box.insertAdjacentHTML('beforeend', `<p id="date">${date}</p>`)
         element.classList.add( 'ai-box' );
-        element.insertAdjacentHTML( "beforeend", "<img src='./assets/img/bot-logo.png' class='picture-ai'><div class='message-ai'>" + message +"</div>" );
+        element.insertAdjacentHTML( "beforeend", '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48.22 48" class="picture-ai"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#7b39eb;}.cls-2,.cls-3,.cls-4{stroke:#fff;stroke-miterlimit:10;}.cls-3{fill:none;}.cls-4{fill:#ffbf85;stroke-width:2px;}</style></defs><g id="fond_icone"><path class="cls-2" d="M33.65,3.72H14.88C7.11,3.72,.81,10.02,.81,17.8v4.21c0,4.82,2.43,9.08,6.13,11.61v7.38l7.73-4.93c.07,0,.15,.01,.22,.01h18.77c7.77,0,14.07-6.3,14.07-14.07v-4.21c0-7.77-6.3-14.07-14.07-14.07Z"/><ellipse class="cls-2" cx="24" cy="42.47" rx="4.19" ry="1.91"/></g><g id="Calque_2"><rect class="cls-3" x="4.88" y="12.15" width="38.17" height="16.98" rx="8.49" ry="8.49"/><g><g><circle class="cls-4" cx="12.76" cy="19.36" r="3.32"/><circle class="cls-4" cx="35.17" cy="19.36" r="3.32"/></g><path class="cls-1" d="M19.02,24.34c3.62,1.81,6.48,2.04,9.89,0-1.93,4.07-7.81,3.77-9.89,0h0Z"/></g></g></svg><div class="message-ai">' + message +"</div>" );
         box.appendChild(element)
         i++;
     }
@@ -45,11 +51,9 @@ function closeChat()
     let button = document.getElementById( 'chat-button' );
 
     chat.classList.add( 'close-chatbox' );
-    chat.classList.remove( 'hide-chatbox' );
-    chat.classList.remove( 'open-chatbox' );
+    chat.classList.remove( 'hide-chatbox', 'open-chatbox' );
     button.classList.remove( 'hide-button' );
-    button.classList.add('button-animation')
-    button.classList.add( 'open-button' );
+    button.classList.add('button-animation', 'open-button')
 }
 
 function reset(){
@@ -63,7 +67,7 @@ function sendMessage()
     let message = document.getElementById( 'message' );
     let box = document.getElementById( 'message-box' );
     let element = document.createElement( 'div' );
-    let request = message.value;
+    let request = escapeHtml( message.value);
 
     if( request != '' )
     {
@@ -125,7 +129,7 @@ function iaResponse( request )
 
     switch ( request.theme ){
         case ( 'presentation' ):
-            message = "Enchanté, je suis arAI, à votre service."
+            message = "Enchanté, je suis Billy, à votre service."
             break;
         case ( 'deconnexion' ):
             message = "À la prochaine ! 😊"
@@ -140,14 +144,54 @@ function iaResponse( request )
         }
     element.classList.add( 'ai-box' );
     document.getElementById('message').setAttribute('readonly', 'readonly')
-    element.insertAdjacentHTML( "beforeend", "<img src='./assets/img/bot-logo.png' class='picture-ai'><div class='message-ai'><img src='./assets/img/loading.png' class='loading'></div>" );
+    element.insertAdjacentHTML( "beforeend", '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48.22 48" class="picture-ai"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#7b39eb;}.cls-2,.cls-3,.cls-4{stroke:#fff;stroke-miterlimit:10;}.cls-3{fill:none;}.cls-4{fill:#ffbf85;stroke-width:2px;}</style></defs><g id="fond_icone"><path class="cls-2" d="M33.65,3.72H14.88C7.11,3.72,.81,10.02,.81,17.8v4.21c0,4.82,2.43,9.08,6.13,11.61v7.38l7.73-4.93c.07,0,.15,.01,.22,.01h18.77c7.77,0,14.07-6.3,14.07-14.07v-4.21c0-7.77-6.3-14.07-14.07-14.07Z"/><ellipse class="cls-2" cx="24" cy="42.47" rx="4.19" ry="1.91"/></g><g id="Calque_2"><rect class="cls-3" x="4.88" y="12.15" width="38.17" height="16.98" rx="8.49" ry="8.49"/><g><g><circle class="cls-4" cx="12.76" cy="19.36" r="3.32"/><circle class="cls-4" cx="35.17" cy="19.36" r="3.32"/></g><path class="cls-1" d="M19.02,24.34c3.62,1.81,6.48,2.04,9.89,0-1.93,4.07-7.81,3.77-9.89,0h0Z"/></g></g></svg><div class="message-ai"><img src="./assets/img/loading.png" class="loading"></div>' );
     box.appendChild( element );
     box.scrollTop += 9999
     setTimeout(()=>{
         element.innerHTML = ''
-        element.insertAdjacentHTML( "beforeend", "<img src='./assets/img/bot-logo.png' class='picture-ai'><div class='message-ai'>" + message +"</div>" );
+        element.insertAdjacentHTML( "beforeend", '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48.22 48" class="picture-ai"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#7b39eb;}.cls-2,.cls-3,.cls-4{stroke:#fff;stroke-miterlimit:10;}.cls-3{fill:none;}.cls-4{fill:#ffbf85;stroke-width:2px;}</style></defs><g id="fond_icone"><path class="cls-2" d="M33.65,3.72H14.88C7.11,3.72,.81,10.02,.81,17.8v4.21c0,4.82,2.43,9.08,6.13,11.61v7.38l7.73-4.93c.07,0,.15,.01,.22,.01h18.77c7.77,0,14.07-6.3,14.07-14.07v-4.21c0-7.77-6.3-14.07-14.07-14.07Z"/><ellipse class="cls-2" cx="24" cy="42.47" rx="4.19" ry="1.91"/></g><g id="Calque_2"><rect class="cls-3" x="4.88" y="12.15" width="38.17" height="16.98" rx="8.49" ry="8.49"/><g><g><circle class="cls-4" cx="12.76" cy="19.36" r="3.32"/><circle class="cls-4" cx="35.17" cy="19.36" r="3.32"/></g><path class="cls-1" d="M19.02,24.34c3.62,1.81,6.48,2.04,9.89,0-1.93,4.07-7.81,3.77-9.89,0h0Z"/></g></g></svg><div class="message-ai">' + message +"</div>" );
         box.appendChild( element );
         document.getElementById('message').removeAttribute('readonly')
         box.scrollTop += 9999
     }, 2500)
+}
+
+function selectColor()
+{
+    let palette = document.getElementById( 'palette' )
+    let r = document.querySelector( ':root' )
+    let rs = getComputedStyle( r )
+
+    switch( palette.value ){
+        case ( 'violet' ):
+            r.style.setProperty('--light-color', '#f0f2f5')
+            r.style.setProperty('--main-color', '#857dff')
+            r.style.setProperty('--main-dark-color', '#7B39EB')
+            r.style.setProperty('--gray-color', '#e4e6eb')
+            break
+        case ( 'vert' ):
+            r.style.setProperty('--light-color', '#f1f5f0')
+            r.style.setProperty('--main-color', '#7fff7d')
+            r.style.setProperty('--main-dark-color', '#12b50d')
+            r.style.setProperty('--gray-color', '#e6ebe4')
+            break
+        case ( 'bleu' ):
+            r.style.setProperty('--light-color', '#f1f5f0')
+            r.style.setProperty('--main-color', '#4538ff')
+            r.style.setProperty('--main-dark-color', '#397aeb')
+            r.style.setProperty('--gray-color', '#e6ebe4')
+            break
+    }
+}
+
+function escapeHtml( text ) {
+    var map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#039;'
+    };
+    
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
