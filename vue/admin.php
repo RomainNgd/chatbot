fbded440c345451a951dc04e3d65f104f05ffebe
julienhatemian <julien-hat.62@hotmail.fr>
2023-02-07 17:36:22 +0100

    interface admin

---
+++ vue/admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <title>Chatbox - Admin</title>
+</head>
+<body id="dashboard">
+    <section class="nav">
+        <div class="brand">
+            <img src="../assets/img/bot-logo.png" alt="logo billy" class="logo-dashboard">
+            <h1 class="title">Billy</h1>
+        </div>
+        <div class="commande">
+            <div class="title-dashboard">
+                <img src="../assets/img/icons8-dashboard-layout-24.png" alt="logo dashboard">
+                <h3>Dashboard</h3>
+            </div>
+
+            <ul class="list-dashboard">
+                <li class="item">Accès</li>
+                <li class="item">Catégorie</li>
+                <li class="item">Keyword</li>
+                <li class="item">Couleurs</li>
+            </ul>
+        </div>
+    </section>
+</body>
+</html>
